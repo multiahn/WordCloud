358342728c7014ac097b1db597c39ad13351d6fa
multiahn <multiahn@gmail.com>
2020-12-09 17:15:12 +0900

    Initialize React Project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // 1
+    entry: {
+        main: ['./src/index.js'] // 2
+    },
+    output: { // 3
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].js' // 4
+    },
+    module: {
+        rules: [
+          { // 1
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            include: path.resolve(__dirname, './src'),
+            loader: 'babel-loader',
+          },
+          { // 2
+            test: /\.html$/,
+            use: [
+              {
+                loader: 'html-loader',
+                options: {
+                  minimize: true,
+                },
+              },
+            ],
+          },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port,
+        open: true,
+      },
+  };
